cc584e5af314545adf50754e1ec4c93eaacd4801
Mahadi Sajjad Neloy <sierra117@qq.com>
2019-12-06 23:01:05 +0800

    Update 01.cpp

--- 001/01.cpp
+++ 001/01.cpp
@@ -1,9 +1,13 @@
-#include <iostream>
-using namespace std;
+#include <iostream>  //standard input-output stream
+using namespace std; //standard namespace. cout, cin and a lot of other things are defined in it.
 
 int main()
 {
-    int num1, num2, num3, smallest, largest; // declaration
+    int num1;     //
+    int num2;     //
+    int num3;     //
+    int smallest; //
+    int largest;  // declaration
 
     cout << "Input three different integers: "; // prompt
     cin >> num1 >> num2 >> num3;                // input
@@ -11,24 +15,24 @@ int main()
     largest = num1; // assume first number is largest
 
     if (num2 > largest) // is num2 larger?
-        largest = num2;
+        largest = num2; //
 
     if (num3 > largest) // is num3 larger?
-        largest = num3;
+        largest = num3; //
 
     smallest = num1; // assume first number is smallest
 
-    if (num2 < smallest)
-        smallest = num2;
+    if (num2 < smallest) //
+        smallest = num2; //
 
-    if (num3 < smallest)
-        smallest = num3;
+    if (num3 < smallest) //
+        smallest = num3; //
 
-    cout << "Sum is " << num1 + num2 + num3
-         << "\nAverage is " << (num1 + num2 + num3) / 3
-         << "\nProduct is " << num1 * num2 * num3
-         << "\nSmallest is " << smallest
-         << "\nLargest is " << largest << endl;
+    cout << "Sum is " << num1 + num2 + num3             //sum of 3 numbers
+         << "\nAverage is " << (num1 + num2 + num3) / 3 //average of 3 numbers
+         << "\nProduct is " << num1 * num2 * num3       //
+         << "\nSmallest is " << smallest                //
+         << "\nLargest is " << largest << endl;         //
 
     return 0;
 }
