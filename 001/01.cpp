2d6c798caf2b20ebaacf2b6ba00aa5e5404364cb
Mahadi Sajjad Neloy <sierra117@qq.com>
2019-12-06 22:42:12 +0800

    Update 01.cpp

--- 001/01.cpp
+++ 001/01.cpp
@@ -2,33 +2,33 @@
 using namespace std;
 
 int main()
- {
- int num1, num2, num3, smallest, largest; // declaration
+{
+    int num1, num2, num3, smallest, largest; // declaration
 
- cout << "Input three different integers: "; // prompt
- cin >> num1 >> num2 >> num3; // input
+    cout << "Input three different integers: "; // prompt
+    cin >> num1 >> num2 >> num3;                // input
 
- largest = num1; // assume first number is largest
+    largest = num1; // assume first number is largest
 
- if ( num2 > largest ) // is num2 larger?
- largest = num2;
+    if (num2 > largest) // is num2 larger?
+        largest = num2;
 
- if ( num3 > largest ) // is num3 larger?
- largest = num3;
+    if (num3 > largest) // is num3 larger?
+        largest = num3;
 
- smallest = num1; // assume first number is smallest
+    smallest = num1; // assume first number is smallest
 
- if ( num2 < smallest )
- smallest = num2;
+    if (num2 < smallest)
+        smallest = num2;
 
- if ( num3 < smallest )
- smallest = num3;
+    if (num3 < smallest)
+        smallest = num3;
 
- cout << "Sum is " << num1 + num2 + num3
- << "\nAverage is " << (num1 + num2 + num3) / 3
- << "\nProduct is " << num1 * num2 * num3
- << "\nSmallest is " << smallest
- << "\nLargest is " << largest << endl;
+    cout << "Sum is " << num1 + num2 + num3
+         << "\nAverage is " << (num1 + num2 + num3) / 3
+         << "\nProduct is " << num1 * num2 * num3
+         << "\nSmallest is " << smallest
+         << "\nLargest is " << largest << endl;
 
- return 0;
- }
+    return 0;
+}
